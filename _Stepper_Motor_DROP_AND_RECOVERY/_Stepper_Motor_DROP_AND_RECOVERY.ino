97ccdd6eb3a7d4c2ecea54ca4b8cc8c2e5841f37
Dario Panzuto <dariopa@users.noreply.github.com>
2019-01-19 15:09:29 +0100

    Create _Stepper_Motor_DROP_AND_RECOVERY.ino

---
+++ _Stepper_Motor_DROP_AND_RECOVERY/_Stepper_Motor_DROP_AND_RECOVERY.ino
@@ -0,0 +1,40 @@
+// Arduino stepper motor control code
+
+#include <Stepper.h> // Include the header file
+
+// ###################################
+// change this to the number of steps on your motor
+float turns = 4; // required revolutions for the package to fall down
+// ###################################
+
+const int revolution = 2048;  // steps per revolution
+int vel = 19; // speed of motor
+int steps = turns * revolution; // steps motor will have to do
+int val; // variable that is entered via Serial Port. 1 for elevating, 2 for descending. 
+
+// create an instance of the stepper class using the steps and pins
+Stepper stepper(revolution, 8, 10, 9, 11); // Defines steps and pins
+
+void setup() {
+  Serial.begin(9600);
+  stepper.setSpeed(vel);
+}
+
+void loop() {
+  if (Serial.available())
+  {
+    val = Serial.parseInt();
+    Serial.println(val);
+
+    if (val == 1)
+    {
+      stepper.step(steps);
+    }
+
+    else if (val == 2)
+    {
+      stepper.step(-steps);
+    }
+  }
+
+}
