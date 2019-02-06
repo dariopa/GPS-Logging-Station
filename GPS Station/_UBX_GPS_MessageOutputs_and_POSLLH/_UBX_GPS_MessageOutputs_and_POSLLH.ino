8a9c96555e613f95f269cecc4ae85b495832a9fe
Dario Panzuto <dariopa@users.noreply.github.com>
2019-02-06 14:33:12 +0100

    New files for communicating with GPS Receiver - finally worked!

---
+++ GPS Station/_UBX_GPS_MessageOutputs_and_POSLLH/_UBX_GPS_MessageOutputs_and_POSLLH.ino
@@ -0,0 +1,123 @@
+#include <SoftwareSerial.h>
+
+const byte rxPin = 8;
+const byte txPin = 9;
+char cha = 'a';
+
+// Connect the GPS RX/TX to arduino pins 8 and 9
+SoftwareSerial serial = SoftwareSerial(rxPin, txPin);
+
+const char UBLOX_INIT[] PROGMEM = {
+  // Disable NMEA
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24, // GxGGA off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B, // GxGLL off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32, // GxGSA off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39, // GxGSV off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x04, 0x40, // GxRMC off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x47, // GxVTG off
+
+  // Disable UBX
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0xB9, //NAV-POSLLH off
+
+  // Enable UBX
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x13, 0xBE, //NAV-POSLLH on
+
+  // Rate
+  0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xE8, 0x03, 0x01, 0x00, 0x01, 0x00, 0x01, 0x39, //(1Hz)
+  // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xD0, 0x07, 0x01, 0x00, 0x01, 0x00, 0xED, 0xBD, // (0.5Hz)
+  // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xB8, 0x0B, 0x01, 0x00, 0x01, 0x00, 0xD9, 0x41, // (0.33Hz)
+
+};
+
+const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
+
+struct NAV_POSLLH {
+  unsigned char cls;
+  unsigned char id;
+  unsigned short len;
+  unsigned long iTOW;
+  long lon;
+  long lat;
+  long height;
+  long hMSL;
+  unsigned long hAcc;
+  unsigned long vAcc;
+};
+
+NAV_POSLLH posllh;
+
+void calcChecksum(unsigned char* CK) {
+  memset(CK, 0, 2);
+  for (int i = 0; i < (int)sizeof(NAV_POSLLH); i++) {
+    CK[0] += ((unsigned char*)(&posllh))[i];
+    CK[1] += CK[0];
+  }
+}
+
+bool processGPS() {
+  static int fpos = 0;
+  static unsigned char checksum[2];
+  const int payloadSize = sizeof(NAV_POSLLH);
+
+  while ( serial.available() ) {
+    byte c = serial.read();
+    if ( fpos < 2 ) {
+      if ( c == UBX_HEADER[fpos] )
+        fpos++;
+      else
+        fpos = 0;
+    }
+    else {
+      if ( (fpos - 2) < payloadSize )
+        ((unsigned char*)(&posllh))[fpos - 2] = c;
+
+      fpos++;
+
+      if ( fpos == (payloadSize + 2) ) {
+        calcChecksum(checksum);
+      }
+      else if ( fpos == (payloadSize + 3) ) {
+        if ( c != checksum[0] )
+          fpos = 0;
+      }
+      else if ( fpos == (payloadSize + 4) ) {
+        fpos = 0;
+        if ( c == checksum[1] ) {
+          return true;
+        }
+      }
+      else if ( fpos > (payloadSize + 4) ) {
+        fpos = 0;
+      }
+    }
+  }
+  return false;
+}
+
+void setup()
+{
+  serial.begin(9600);
+  Serial.begin(9600);
+  // initialize both serial ports:
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  // send configuration data in UBX protocol
+  for (int i = 0; i < sizeof(UBLOX_INIT); i++) {
+    serial.write( pgm_read_byte(UBLOX_INIT + i) );
+    delay(5); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+  }
+}
+
+void loop() {
+  if ( processGPS() ) {
+    Serial.print("I'm here: ");
+    Serial.print("iTOW:");      Serial.print(posllh.iTOW);
+    Serial.print(" lat/lon: "); Serial.print(posllh.lat / 10000000.0f); Serial.print(","); Serial.print(posllh.lon / 10000000.0f);
+    Serial.print(" height: ");  Serial.print(posllh.height / 1000.0f);
+    Serial.print(" hMSL: ");    Serial.print(posllh.hMSL / 1000.0f);
+    Serial.print(" hAcc: ");    Serial.print(posllh.hAcc / 1000.0f);
+    Serial.print(" vAcc: ");    Serial.print(posllh.vAcc / 1000.0f);
+    Serial.println();
+  }
+}
