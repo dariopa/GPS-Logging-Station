39ae458c4c72334a6c2e4c5d94c80dafaf193549
Dario Panzuto <dariopa@users.noreply.github.com>
2019-03-05 15:12:45 +0100

    Update __GPS_Station_V1.ino

--- GPS Station/__GPS_Station_V1/__GPS_Station_V1.ino
+++ GPS Station/__GPS_Station_V1/__GPS_Station_V1.ino
@@ -46,95 +46,34 @@ OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with a
 DallasTemperature sensors(&oneWire); // Pass oneWire reference to Dallas Temperature.
 
 // SD CARD
-File GPSFile; // Declare file where GPS data will be written onto. 
+File GPSFile; // Declare file where GPS data will be written onto.
 const int CS = 10; // Chip Select Pin for communication with SD card.
 
 // TPL5110
 const int DonePin = 4; // Signal to timer TPL5110
-int counter = 0; // Counts how many positions have been logged and stored. 
-int measPoints = 20; // Define how many measurment points you want to collect. 
+unsigned long startTime;
+unsigned long currTime;
+float measTime = 2; // in Minutes!
 // ###################################################################################################
 
-// Declare structure for NAV-POSLLH message
-const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
-
-struct NAV_POSLLH {
-  unsigned char cls;
-  unsigned char id;
-  unsigned short len;
-  unsigned long iTOW;
-  long lon;
-  long lat;
-  long height;
-  long hMSL;
-  unsigned long hAcc;
-  unsigned long vAcc;
-};
-
-NAV_POSLLH posllh;
-
-void calcChecksum(unsigned char* CK) {
-  memset(CK, 0, 2);
-  for (int i = 0; i < (int)sizeof(NAV_POSLLH); i++) {
-    CK[0] += ((unsigned char*)(&posllh))[i];
-    CK[1] += CK[0];
-  }
-}
-
-bool processGPS() {
-  static int fpos = 0;
-  static unsigned char checksum[2];
-  const int payloadSize = sizeof(NAV_POSLLH);
-
-  while ( serial.available() ) {
-    byte c = serial.read();
-    if ( fpos < 2 ) {
-      if ( c == UBX_HEADER[fpos] )
-        fpos++;
-      else
-        fpos = 0;
-    }
-    else {
-      if ( (fpos - 2) < payloadSize )
-        ((unsigned char*)(&posllh))[fpos - 2] = c;
-
-      fpos++;
-
-      if ( fpos == (payloadSize + 2) ) {
-        calcChecksum(checksum);
-      }
-      else if ( fpos == (payloadSize + 3) ) {
-        if ( c != checksum[0] )
-          fpos = 0;
-      }
-      else if ( fpos == (payloadSize + 4) ) {
-        fpos = 0;
-        if ( c == checksum[1] ) {
-          return true;
-        }
-      }
-      else if ( fpos > (payloadSize + 4) ) {
-        fpos = 0;
-      }
-    }
-  }
-  return false;
-}
 
 void setup() {
-  Serial.begin(9600); // Start serial port
-
-  // GPS RECEIVER
-  serial.begin(9600); // Start serial port with GPS receiver
   // initialize both serial ports:
+  Serial.begin(9600); // Start serial port
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
+  serial.begin(9600); // Start serial port with GPS receiver
 
+  delay(1000);
+
+  // GPS RECEIVER
   // send configuration data in UBX protocol
   for (int i = 0; i < sizeof(UBLOX_INIT); i++) {
     serial.write( pgm_read_byte(UBLOX_INIT + i) );
-    delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+    delay(5); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
   }
+
+  delay(1000);
 
   // TEMPERATURE SENSOR
   sensors.begin(); // Start up the library. IC Default 9 bit. If you have troubles consider upping it up to 12. Ups the the delay giving the IC more time to process the temperature measurement
@@ -142,6 +81,8 @@ void setup() {
   temp = sensors.getTempCByIndex(0); // Read the temperature
   Serial.print("Temperature for Device is: ");
   Serial.println(temp);
+
+  delay(1000);
 
   // SD CARD
   // Initialize SD Card
@@ -151,45 +92,39 @@ void setup() {
     return;
   }
   // Write separation line in .txt file
-  GPSFile = SD.open("GPS.txt", FILE_WRITE);
+  GPSFile = SD.open("ROV.bin", FILE_WRITE);
   GPSFile.println("-----------------------------------");
   GPSFile.println("");
-  GPSFile.close();
+  GPSFile.flush();
+
+  delay(1000);
 
   // TPL5110
-  pinMode(DonePin,OUTPUT);
+  pinMode(DonePin, OUTPUT);
   digitalWrite(DonePin, LOW);
+
+  delay(200);
+
+  startTime = millis();
 }
 
 void loop() {
-  if ( processGPS() ) {
-    // Print data on console
-    Serial.print("I'm here: ");
-    Serial.print("iTOW: ");
-    Serial.print(posllh.iTOW);
-    Serial.print(" lat/lon: "); 
-    Serial.print(posllh.lat / 10000000.0f);
-    Serial.print(","); 
-    Serial.print(posllh.lon / 10000000.0f);
-    Serial.print(" height: ");  
-    Serial.print(posllh.height / 1000.0f);
-    Serial.println();
-
-    // Store data on SD card
-    GPSFile = SD.open("GPS.txt", FILE_WRITE);
-    GPSFile.print("iTOW: ");
-    GPSFile.println(posllh.iTOW);
-    GPSFile.print("lat/lon: ");
-    GPSFile.print(posllh.lat / 10000000.0f); 
-    GPSFile.print(", ");
-    GPSFile.println(posllh.lon / 10000000.0f);
-    GPSFile.print("height: ");
-    GPSFile.println(posllh.height / 1000.0f);
-    GPSFile.close();
-
-    counter = counter + 1;
-    if (counter == measPoints) {
-      digitalWrite(DonePin, HIGH); // toggle DONE so TPL knows to cut power!
+  if (serial.available()) {
+    int ci = serial.read();
+    if (ci == -1) {
+      Serial.print("Reading failed!");
+      return;
     }
+    char c = ci;
+    Serial.write(c);
+    GPSFile.write(c);
+    GPSFile.flush();
+  }
+  
+  currTime = millis();
+  if (currTime - startTime >= (measTime * 60 * 1000)) {
+    GPSFile.close();
+    delay(20);
+    digitalWrite(DonePin, HIGH); // toggle DONE so TPL5110 knows to cut power!
   }
 }
