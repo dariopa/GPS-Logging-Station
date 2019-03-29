bace7c277bd6ab688e7cf1cf2323d719a979ae46
dariopa <dariopa@ethz.ch>
2019-03-29 15:56:31 +0100

    wrote python file to concatenate binary files

---
+++ Postprocessing/Concat_Files.py
@@ -0,0 +1,12 @@
+import os
+import glob
+import numpy as np
+import fnmatch
+
+callFolder = 'Raw_Data/'
+
+nrFiles = len(fnmatch.filter(os.listdir(callFolder), '*.BIN'))
+
+for i in range(0, nrFiles):
+    for f in glob.glob(os.path.join(callFolder, 'ROV' + str(i+1) + '.BIN')):
+        os.system('cat '+f+' >> Concat_Data.bin')
