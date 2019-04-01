d7b4707a76476baccd31471233bfccbdbf2d22b6
Dario Panzuto <dariopa@users.noreply.github.com>
2019-04-01 15:40:50 +0200

    deleted dynamixel for arduino, added dynamixel with OpenCM9.04

---
+++ Drop Mechanism/dynamixel_test/dynamixel_test.ino
@@ -0,0 +1,19 @@
+int led_pin = 14;
+
+void setup() {
+  // Set up the built-in LED pin as an output:
+  pinMode(led_pin, OUTPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  int i;
+
+  digitalWrite(led_pin, HIGH);  // set to as HIGH LED is turn-off
+  Serial.println("led_off");
+  delay(1000);                   // Wait for 0.1 second
+  digitalWrite(led_pin, LOW);   // set to as LOW LED is turn-on
+  Serial.println("led_on");
+  delay(1000);                   // Wait for 0.1 second
+}
