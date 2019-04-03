31c572591fbae214c2cdf8e7ec39cf5bdea3ebe1
Dario Panzuto <dariopa@users.noreply.github.com>
2019-04-03 17:49:12 +0200

    Update dynamixel_test.ino

--- Drop Mechanism/dynamixel_test/dynamixel_test.ino
+++ Drop Mechanism/dynamixel_test/dynamixel_test.ino
@@ -1,19 +1,43 @@
-int led_pin = 14;
+// Test motor wheel mode
 
-void setup() {
-  // Set up the built-in LED pin as an output:
-  pinMode(led_pin, OUTPUT);
+#include "DynamixelMotor.h"
 
-  Serial.begin(115200);
+// id of the motor
+const uint8_t id=1;
+// speed, between -1023 and 1023
+int16_t speed=512;
+// communication baudrate
+const long unsigned int baudrate = 1000000;
+
+// hardware serial without tristate buffer
+// see blink_led example, and adapt to your configuration
+HardwareDynamixelInterface interface(Serial);
+
+DynamixelMotor motor(interface, id);
+
+void setup()
+{ 
+  interface.begin(baudrate);
+  delay(100);
+  
+  // check if we can communicate with the motor
+  // if not, we turn the led on and stop here
+  uint8_t status=motor.init();
+  if(status!=DYN_STATUS_OK)
+  {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    while(1);
+  }
+
+  motor.enableTorque();  
+  motor.wheelMode();
 }
 
-void loop() {
-  int i;
-
-  digitalWrite(led_pin, HIGH);  // set to as HIGH LED is turn-off
-  Serial.println("led_off");
-  delay(1000);                   // Wait for 0.1 second
-  digitalWrite(led_pin, LOW);   // set to as LOW LED is turn-on
-  Serial.println("led_on");
-  delay(1000);                   // Wait for 0.1 second
+//change motion direction every 5 seconds
+void loop() 
+{
+  motor.speed(speed);
+  speed=-speed;
+  delay(5000);
 }
