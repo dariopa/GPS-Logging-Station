a3312b4f4a3273a79aae4631b0e9d674d46cfc62
dariopa <dariopa@ethz.ch>
2019-04-24 17:21:20 +0200

    updated _GPS_ROVER.ino

--- GPS Station/_GPS_ROVER/_GPS_ROVER.ino
+++ GPS Station/_GPS_ROVER/_GPS_ROVER.ino
@@ -8,10 +8,12 @@
 #include <SD.h>
 
 // Declare measurment time
-float measTime = 2; // in Minutes!
+float measurment_time = 2; // in Minutes!
+bool bms_state = false; // if bms connected, then true. Otherwise, false.
 
 // ###################################################################################################
-const char UBLOX_INIT_POSLLH[] PROGMEM = {
+
+const char UBLOX_INIT[] PROGMEM = {
   // Disable NMEA
   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24, // GxGGA off
   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B, // GxGLL off
@@ -25,17 +27,34 @@ const char UBLOX_INIT_POSLLH[] PROGMEM = {
   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x38, // RXM-SFRBX off
   0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0xB9, // NAV-POSLLH off
 
-  // Enable POSLLH
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x13, 0xBE, // NAV-POSLLH on
-
   // Rate
   // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xE8, 0x03, 0x01, 0x00, 0x01, 0x00, 0x01, 0x39, //(1Hz)
   0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xD0, 0x07, 0x01, 0x00, 0x01, 0x00, 0xED, 0xBD, // (0.5Hz)
   // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xB8, 0x0B, 0x01, 0x00, 0x01, 0x00, 0xD9, 0x41, // (0.33Hz)
-
 };
 
-const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
+const char UBLOX_INIT_RAWX[] PROGMEM = {
+  // Disable UBX
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x46, // RXM-RAWX off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x38, // RXM-SFRBX off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0xB9, // NAV-POSLLH off
+
+  // Enable UBX
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x15, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x27, 0x4B, // RXM-RAWX on
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x25, 0x3D, // RXM-SFRBX on
+};
+
+const char UBLOX_INIT_POSLLH[] PROGMEM = {
+  // Disable UBX
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x46, // RXM-RAWX off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x38, // RXM-SFRBX off
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0xB9, // NAV-POSLLH off
+
+  // Enable POSLLH
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x13, 0xBE, // NAV-POSLLH on
+};
+
+const unsigned char UBX_HEADER_POSLLH[] = { 0xB5, 0x62 };
 
 struct NAV_POSLLH {
   unsigned char cls;
@@ -52,61 +71,35 @@ struct NAV_POSLLH {
 
 NAV_POSLLH posllh;
 
-
-const char UBLOX_INIT_RAWX[] PROGMEM = {
-  // Disable NMEA
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x24, // GxGGA off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x2B, // GxGLL off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x32, // GxGSA off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x39, // GxGSV off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x04, 0x40, // GxRMC off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x05, 0x47, // GxVTG off
-
-  // Disable UBX
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x46, // RXM-RAWX off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x38, // RXM-SFRBX off
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0xB9, // NAV-POSLLH off
-
-  // Enable UBX
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x15, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x27, 0x4B, // RXM-RAWX on
-  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0x02, 0x13, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x25, 0x3D, // RXM-SFRBX on
-
-  // Rate
-  0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xE8, 0x03, 0x01, 0x00, 0x01, 0x00, 0x01, 0x39, //(1Hz)
-  // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xD0, 0x07, 0x01, 0x00, 0x01, 0x00, 0xED, 0xBD, // (0.5Hz)
-  // 0xB5, 0x62, 0x06, 0x08, 0x06, 0x00, 0xB8, 0x0B, 0x01, 0x00, 0x01, 0x00, 0xD9, 0x41, // (0.33Hz)
-
-};
-
 // GPS RECEIVER
-const int bufLen = 2000;
+const int buffer_length = 2000;
 
 // TEMPERATURE SENSOR
-#define ONE_WIRE_BUS 5 // Pin 5 on Board. 
-float temp; // Variable to store temperature value.
-OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+float temperature; // Variable to store temperature value.
+OneWire oneWire(5); // Setup a oneWire instance to communicate with any OneWire devices on pin 5 on Arduino Board (not just Maxim/Dallas temperature ICs)
 DallasTemperature sensors(&oneWire); // Pass oneWire reference to Dallas Temperature.
 
 // VOLTAGE MEASUREMENT
-int analogValue; // Analog value that Arduino reads
-const float maxVoltageBattery = 4.2; // Maximum voltage on battery when charged
-const float maxVoltageArduino = 3; // Maximum voltage Arduino should read
-const float lowVolt = -3.35; // change sign from - to + in the final version!
-const float vpp = maxVoltageBattery / 1023; // Volts per point <-> accuracy of measurement
-const float voltageRatio = maxVoltageBattery / maxVoltageArduino; // Ratio to mathematically upscale voltage
-float voltage; // Real voltage
+int analog_value; // Analog value that Arduino reads
+const float kMaxVoltageBattery = 4.2; // Maximum voltage on battery when charged
+const float kMaxVoltageArduino = 3.3; // Maximum voltage Arduino should read
+const float kLowVoltage = 3.3; // Lowest Voltage where TPL5110 has to be toggled
+const float kVpp = kMaxVoltageBattery / 1023; // Volts per point <-> accuracy of measurement
+const float kVoltageRatio = kMaxVoltageBattery / kMaxVoltageArduino; // Ratio to mathematically upscale voltage
+float real_voltage; // Real voltage
 
 // SD CARD
+const int kChipSelect = 10; // Chip Select Pin for communication with SD card.
 File bmsFile; // Declare file where BMS data will be written onto.
 File gpsFile; // Declare file where GPS data will be written onto.
 File root;
-const int CS = 10; // Chip Select Pin for communication with SD card.
 
 // TPL5110
-const int donePin = 4; // Signal to timer TPL5110
-unsigned long startTime;
-unsigned long currTime;
-unsigned long weekTime;
+const int kDonePin = 4; // Signal to timer TPL5110
+unsigned long start_time;
+unsigned long current_time;
+unsigned long week_time;
+
 // ###################################################################################################
 
 void setup() {
@@ -114,167 +107,169 @@ void setup() {
   Serial.begin(9600); // Start serial port
   Serial1.begin(9600); // Start serial port with GPS receiver
   Serial2.begin(9600); // Start serial port with XBEE module
-  delay(5);
   pinMode(LED_BUILTIN, OUTPUT);
-
+  delay(20);
 
   // Initialise TPL5110
-  pinMode(donePin, OUTPUT);
-  digitalWrite(donePin, LOW);
-  delay(5);
+  pinMode(kDonePin, OUTPUT);
+  digitalWrite(kDonePin, LOW);
+  delay(20);
 
-  sdInit(); // Initialize SD Card
-  delay(500);
-  
-  gpsInit(); // send configuration data to get NAV-POSLLH
-  delay(500);
+  SdInit(); // Initialize SD Card
+  delay(20);
 
-  // Send Heartbeats to homebase, i.e. send NAV-POSLLH to homebase via XBEE
+  GpsInit(); // send configuration for GPS initialisation
+  delay(20);
+
+  PosllhConfig(); // send configuration data to get NAV-POSLLH
+  delay(8000); // wait 8 seconds until position lock on GPS receiver
+
+  // Store weektime for BMS
   int counter = 0;
   while (true) {
-    if (processGPS() ) {
-      // Send just fixed position via Xbee
-      Serial2.print("iTOW: ");
-      Serial2.print(posllh.iTOW);
-      Serial2.print(" , lat/lon :");
-      Serial2.print(posllh.lat / 10000000., 9);
-      Serial2.print(", ");
-      Serial2.print(posllh.lon / 10000000., 9);
-      Serial2.print(", height: ");
-      Serial2.println(posllh.height / 1000., 6);
+    if (ProcessGPS() ) {
       counter += 1;
+      delay(20);
     }
-    if (counter == 10) {
-      weekTime = posllh.iTOW;
+    if (counter == 5) {
+      week_time = posllh.iTOW;
       break;
     }
-    // If for any reason processGPS gets stuck here, it will switch off after measTime
-    currTime = millis(); // Track time
-    if (currTime - startTime > measTime * 60 * 1000) { // if measTime is exceeded, toggle TPL5110
-      Serial2.println("stuck in processPGS");
+    // If for any reason ProcessGPS gets stuck here, it will switch off after measurment_time
+    current_time = millis(); // Track time
+    if (current_time - start_time > measurment_time * 60 * 1000) { // if measurment_time is exceeded, toggle TPL5110
+      Serial2.println("stuck in processGPS");
       delay(20);
-      digitalWrite(donePin, HIGH); // toggle TPL5110
+      digitalWrite(kDonePin, HIGH); // toggle TPL5110
     }
   }
-  delay(500);
+  delay(20);
 
   // Battery Management System
-  if ( bms() ) {
-    for (int i = 0; i < 5; i++) {
-      Serial2.println("LOW BATTERY VOLTAGE - PICK ME UP");
-    }
-    Serial2.flush();
-    delay(5);
-    digitalWrite(donePin, HIGH); // switch off whole system
+  if ( Bms() ) {
+    Serial2.println("LOW BATTERY VOLTAGE - PICK ME UP");
+    delay(20);
+    digitalWrite(kDonePin, HIGH); // switch off whole system
   }
-  delay(500);
+  delay(20);
 
-  gpsConfig(); // send configuration data in UBX protocol to receive RAWX and SFRBX
-  delay(500);
+  RawxConfig(); // send configuration data in UBX protocol to receive RAWX and SFRBX
+  delay(20);
 
   // Open GPS File
   root = SD.open("/");
-  openFile(root);
+  OpenFile(root);
   delay(500);
 
-  startTime = millis();
+  start_time = millis();
 }
 
 void loop() {
-  char buf[bufLen]; // declare a buffer
-  int bufIndex = 0; // declare buffer index
-  while (Serial1.available()) { // while GPS receiver transmitts bytes, write them into buffer
-    buf[bufIndex] = (char) Serial1.read(); // Store byte into buffer
-    Serial2.write(buf[bufIndex]); // Send byte via xbee to homebase
-    bufIndex += 1;
-    if (bufIndex > bufLen) { // if more data available on Serial1 than bufLen, delete message
-      bufIndex = 0;
+  char rawx_buffer[buffer_length]; // declare a buffer
+  int buffer_index = 0; // declare buffer index
+  while (Serial1.available()) { // while GPS receiver transmits bytes, write them into buffer
+    rawx_buffer[buffer_index] = (char) Serial1.read(); // Store byte into buffer
+    Serial2.write(rawx_buffer[buffer_index]); // Send byte via xbee to homebase
+    buffer_index++;
+    if (buffer_index > buffer_length) { // if more data available on Serial1 than buffer_length, delete message
+      buffer_index = 0;
       break;
     }
   }
 
-  if (bufIndex != 0) {
-    gpsFile.write(buf , bufIndex); // when GPS receiver is done transmitting data, store it on microSD
+  if (buffer_index != 0) {
+    gpsFile.write(rawx_buffer , buffer_index); // when GPS receiver is done transmitting data, store it on microSD
     gpsFile.flush();
   }
 
-  currTime = millis(); // measure current time
-  if (currTime - startTime > measTime * 60 * 1000) { // if measTime has been exceeded, toggle TPL5110
+  current_time = millis(); // measure current time
+  if (current_time - start_time >  measurment_time * 60 * 1000) { // if measurment_time has been exceeded, toggle TPL5110
     gpsFile.close();
     delay(20);
-    digitalWrite(donePin, HIGH); // toggle TPL5110
+    digitalWrite(kDonePin, HIGH); // toggle TPL5110
   }
 }
 
 // ###################################################################################################
 
-void sdInit() {
-  pinMode(CS, OUTPUT);
-  if (!SD.begin(CS)) {
+void SdInit() {
+  pinMode(kChipSelect, OUTPUT);
+  if (!SD.begin(kChipSelect)) {
     Serial.println("initialisation failed");
     return;
   }
   Serial.println("initialisation succeeded");
 }
 
-void openFile(File dir) {
-  int fileCount = -1; // -1 as bms file will also be counted.
+void OpenFile(File dir) {
+  int file_count = -1; // -1 as bms-file will also be counted.
   while (true) {
     File entry =  dir.openNextFile();
     if (! entry) {
       // no more files
-      gpsFile = SD.open("ROV" + String(fileCount) + ".bin", FILE_WRITE);
+      gpsFile = SD.open("ROV" + String(file_count) + ".bin", FILE_WRITE);
       break;
     }
     else {
-      fileCount += 1;
+      file_count += 1;
     }
   }
 }
 
-void gpsInit() {
+void GpsInit() {
+  for (int i = 0; i < sizeof(UBLOX_INIT); i++) {
+    Serial1.write( pgm_read_byte(UBLOX_INIT + i) );
+    delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+  }
+}
+
+void PosllhConfig() {
   for (int i = 0; i < sizeof(UBLOX_INIT_POSLLH); i++) {
     Serial1.write( pgm_read_byte(UBLOX_INIT_POSLLH + i) );
     delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
   }
 }
 
-void gpsConfig() {
+void RawxConfig() {
   for (int i = 0; i < sizeof(UBLOX_INIT_RAWX); i++) {
     Serial1.write( pgm_read_byte(UBLOX_INIT_RAWX + i) );
     delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
   }
 }
 
-bool bms() {
+bool Bms() {
   bmsFile = SD.open("bms.txt", FILE_WRITE);
 
-  // TEMPERATURE SENSOR
-  sensors.begin(); // Start up the library. IC Default 9 bit. If you have troubles consider upping it up to 12. Ups the the delay giving the IC more time to process the temperature measurement
+  // temperatureERATURE SENSOR
+  sensors.begin(); // Start up the library. IC Default 9 bit.
   sensors.requestTemperatures(); // Send the command to get temperatures
-  temp = sensors.getTempCByIndex(0); // Read the temperature
+  temperature = sensors.getTempCByIndex(0); // Read the temperature
   delay(5);
 
   // VOLTAGE
-  analogValue = analogRead(A0);
-  voltage = (analogValue * vpp) * voltageRatio;
+  analog_value = analogRead(A0);
+  real_voltage = (analog_value * kVpp) * kVoltageRatio;
   delay(5);
 
   // STORE DATA
   bmsFile.print("iTOW: ");
-  bmsFile.print(weekTime);
+  bmsFile.print(week_time);
   bmsFile.print(", Temperature: ");
-  bmsFile.print(temp);
+  bmsFile.print(temperature);
   bmsFile.print("°C, Voltage: ");
-  bmsFile.print(voltage);
+  bmsFile.print(real_voltage);
   bmsFile.println("V");
   bmsFile.close();
   delay(5);
 
-  if (voltage < lowVolt) {
+  if (bms_state == false) { // if bms is not connected, i.e. bms_state = false, then return false
+    return false;
+  }
+
+  if (real_voltage < kLowVoltage) { // if voltage is below voltage tolerance, then return true to toggle TPL5110
     return true;
   }
-  else {
+  else { // if voltage is above voltage tolerance, program can continue
     return false;
   }
 }
@@ -287,7 +282,7 @@ void calcChecksum(unsigned char* CK) {
   }
 }
 
-bool processGPS() {
+bool ProcessGPS() {
   static int fpos = 0;
   static unsigned char checksum[2];
   const int payloadSize = sizeof(NAV_POSLLH);
@@ -295,7 +290,7 @@ bool processGPS() {
   while ( Serial1.available() ) {
     byte c = Serial1.read();
     if ( fpos < 2 ) {
-      if ( c == UBX_HEADER[fpos] )
+      if ( c == UBX_HEADER_POSLLH[fpos] )
         fpos++;
       else
         fpos = 0;
@@ -325,4 +320,10 @@ bool processGPS() {
     }
   }
   return false;
+}
+
+void test() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(5000);
+  digitalWrite(LED_BUILTIN, LOW);
 }
