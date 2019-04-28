7e6c9b6a71870572188275aa4f366b7b7d3efdf3
dariopa <dariopa@ethz.ch>
2019-04-28 11:26:10 +0200

    completed corrections from Michael, put everything in classes

---
+++ GPS Station/_GPS_ROVER_V2/GPSLibrary.cpp
@@ -0,0 +1,21 @@
+#include "DropRecovery.h"
+
+SettingGPS::SettingGPS() {
+  // Anything required when instantiating an object, goes here.
+}
+
+void SettingGPS::GpsInit() {
+  for (int i = 0; i < sizeof(UBLOX_INIT); i++) {
+    Serial1.write( pgm_read_byte(UBLOX_INIT + i) );
+    delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+  }
+  delay(8000); // wait 8 seconds until position lock on GPS receiver
+}
+
+void SettingGPS::RawxConfig() {
+  for (int i = 0; i < sizeof(UBLOX_INIT_RAWX); i++) {
+    Serial1.write( pgm_read_byte(UBLOX_INIT_RAWX + i) );
+    delay(10); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+  }
+  delay(20);
+}
